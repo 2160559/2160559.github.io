be1de9633e95082004a0979924dc7f7dd9dc7a78
2168920 <2168920@slu.edu.ph>
2018-05-11 23:02:23 +0800

    Added view profile page

--- client/profile.php
+++ client/profile.php
@@ -5,94 +5,84 @@ require_once('includes/head.inc.php') ?>
 <body>
 <?php
 include_once('includes/nav.inc.php');
-include_once('functions.php');
-include_once('User.php');
-include_once('getUser.php');
-include_once('includes/db.inc.php');
 ?>
-<div class="container" >
 
-    <?php
-    if (isset($_REQUEST['username'])) {
-        $current_user->setUsername($_REQUEST['username']);
-        $current_user->setFirstName($_REQUEST['f_name']);
-        $current_user->setLastName($_REQUEST['l_name']);
-        $current_user->setBirthDate($_REQUEST['birthdate']);
-        $current_user->setPhoneNumber($_REQUEST['phone']);
-        $query = "UPDATE `users` SET `username`='" . $current_user->getUsername() . "',`f_name`='" . $current_user->getFirstName() . "',
-      `l_name`='" . $current_user->getLastName() . "',`birthdate`='" . $current_user->getBirthDate() . "',
-      `phone_number`='" . $current_user->getPhoneNumber() . "' WHERE `id` = '" . $current_user->getUserId() . "'";
-        $result = mysqli_query($con, $query) or die("an error occurred");
-    } ?>
-    <div class="row m-4 pb-5 justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><h1>Edit Profile</h1></div>
-                <div class="card-body">
-
-                    <form action="uploadfile.php" method="POST" enctype="multipart/form-data" target="_blank">
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Add/Change<br/>Profile Photo</label>
-                            <div class="col-md-6">
-                                <p><input type="file" name="file"></p>
-                                <input class="btn btn-secondary btn-sm" type="submit" value="Upload">
-                                <input class="btn btn-outline-danger btn-sm" type="submit" name="submit" value="Remove Photo"/>
-                            </div>
-                        </div>
-                    </form>
-
-                    <form name="registration" action="" method="post">
-
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Username</label>
-                            <div class="col-md-6">
-                                <input class="form-control" type="text" name="username"
-                                       value="<?php echo $current_user->getUsername() ?>"
-                                       required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">First Name</label>
-                            <div class="col-md-6">
-                                <input class="form-control" type="text" name="f_name" value="<?php
-                                echo $current_user->getFirstName() ?>" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Last Name</label>
-                            <div class="col-md-6">
-                                <input class="form-control" type="text" name="l_name" value="<?php
-                                echo $current_user->getLastName() ?>" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Birthdate</label>
-                            <div class="col-md-6">
-                                <input class="form-control" type="date" name="birthdate" value="<?php
-                                echo $current_user->getBirthDate() ?>" required>
-                                <p><input type="checkbox" name="" value="hide"> Hide Birthday</p>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-4 col-form-label text-md-right">Phone Number</label>
-                            <div class="col-md-6">
-                                <input class="form-control" type="tel" name="phone" value="<?php echo
-                                $current_user->getPhoneNumber() ?>" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <input class="btn-primary btn" type="submit" name="submit"  value="Save Changes"/>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="container">
+    <div class="row profile">
+      <!-- SIDE PROFILE -->
+		<div class="col-md-3">
+			<div class="profile-sidebar">
+				<!-- SIDEBAR USERPIC -->
+				<div class="profile-userpic">
+                  <img src="//placehold.it/150" class="mx-auto img-fluid img-circle d-block" alt="avatar">
+				</div>
+				<!-- END SIDEBAR USERPIC -->
+				<!-- SIDEBAR USER TITLE -->
+				<div class="profile-usertitle">
+					<div class="profile-usertitle-name">
+						My Name
+					</div>
+					<div class="profile-usertitle-username">
+						@username
+					</div>
+                    <div class="profile-usertitle-birthday">
+						Birthday: Month 00, 0000
+					</div>
+				</div>
+				<!-- END SIDEBAR USER TITLE -->
+				<!-- SIDEBAR BUTTONS
+				<div class="profile-userbuttons">
+					<button type="button" class="btn btn-info btn-sm">Message</button>
+				</div>
+				 -->
+			</div>
+		</div>
+      <!-- END SIDE PROFILE -->
+		<div class="col-md-9">
+            <div class="profile-content">			   
+              <div class="bd-example bd-example-tabs">
+                <nav class="nav nav-tabs" id="nav-tab" role="tablist">
+                  <p class="profile-usertitle-name">Booking History</p>
+                </nav>      
+                    <br>
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th scope="col">Check-in</th>
+                          <th scope="col">Check-out</th>
+                          <th scope="col">Place</th>
+                          <th scope="col">Amount Paid</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>Larry</td>
+                          <td>the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    </div>
+                
+              </div>
             </div>
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
+
     <?php require_once ('includes/footer.inc.php')?>
 </body>
 </html>
